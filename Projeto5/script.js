546de3b82033c805ceea983eb167b9f92c0a8227
Luiz Filipe <91493368+luizfilipe7@users.noreply.github.com>
2021-11-03 14:34:40 -0300

    Primeiro commit

---
+++ Projeto5/script.js
@@ -0,0 +1,33 @@
+var frmNome 
+var frmVeiculo
+var frmPreco;
+var mostrar
+
+btnCalcular.addEventListener('click', function(){
+    frmNome = nome.value
+    frmVeiculo = veiculo.value
+    frmPreco = preco.value
+    mostrar = document.getElementById('mostrarResultado')
+
+    entrada = frmPreco /2
+    parcela = parseFloat(((frmPreco/2)/60)*1.7).toFixed(2)
+
+
+    // Link usado para conhecimento em formatar float para Real
+    // https://www.horadecodar.com.br/2020/09/01/formatar-moeda-brasileira-em-javascript-float-para-real/
+
+
+    entradaBrl = entrada.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
+    parcelaBrl = parcela.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
+    
+
+    mostrar.innerHTML = `
+    <div> Prezado  ${frmNome}, o valor da entrada do veículo </div>
+    <div Class="carro"> ${frmVeiculo} </div>
+    <div> Deverá ser de  ${entradaBrl} </div>
+    <div> + 60x de ${parcelaBrl} </div>
+    `
+
+})
+
+
