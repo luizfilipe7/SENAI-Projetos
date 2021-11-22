fe648eb0b31ad6b7b0ca93b393587da319fa4070
Luiz Filipe work <luiz.amaral747@hotmail.com>
2021-11-22 17:46:24 -0300

    End of code

--- Projeto6/app.js
+++ Projeto6/app.js
@@ -1,21 +1,25 @@
 const listaDeEspera = []
 
-
-function adicionarLista() {
+function adicionarLista(urgencia) {
     console.log("add lista")
     let paciente = document.querySelector("#paciente").value //pega o nome na tela
     let ulLista = document.querySelector("#listaEspera") //sai com a lista
 
-    //verifica prenchimento do campo
     if (paciente == ""){
-        alert("Preencha o nome corretamente!");
-        return false;
+    alert("Preencha o nome corretamente!");
+    return false;
+    }
+    
+
+
+    //verifica se variável "urgência" é true. Se sim dá unshift, se não da push.
+    if(urgencia){
+        listaDeEspera.unshift(paciente)
+    }else{
+        listaDeEspera.push(paciente)
     }
 
-
-
     //adiciona paciente no array
-    listaDeEspera.push(paciente)
     
     let listaTela = ""
 
@@ -31,18 +35,37 @@ function adicionarLista() {
     //manda o foco da tela para o campo nome paciente
     document.querySelector("#paciente").focus()
 }
-
-
-
-function adicionarUrgencia() {
-    console.log("adicionarUrgencia")
-}
-
+//arrow function e função anônima
 const atender = () => {
-    console.log("atender")
+    let paciente = document.querySelector("#paciente").value //pega o nome na tela
+    let ulLista = document.querySelector("#listaEspera") //sai com a lista
+    let nomeAtendimento = document.querySelector("#nomePessoaAtendimento")
+
+    if(listaDeEspera.length === 0){
+        alert("Não existem pacientes na lista de espera")
+        document.querySelector("#paciente").focus()
+    return
+    }
+
+    //removendo o primeiro paciente e guardando na let pacienteEmAtendimento
+    let pacienteEmAtendimento = listaDeEspera.shift()
+    console.log(pacienteEmAtendimento)
+
+    nomeAtendimento.innerHTML = pacienteEmAtendimento
+    document.querySelector("#alertAtendimento").style = "Display:block"
+
+  
+    let listaTela = ""
+
+    listaDeEspera.forEach(function (item, indice){
+    listaTela += ` <li class="list-group-item">${indice+1} - ${item} </li>`
+    })
+
+    ulLista.innerHTML = listaTela
+
 }
 
 
-btnAdd.addEventListener("click", adicionarLista);
-btnUrg.addEventListener("click", adicionarUrgencia);
+btnAdd.addEventListener("click", () => adicionarLista(false));
+btnUrg.addEventListener("click", () => adicionarLista(true));
 btnAtender.addEventListener("click", atender);
